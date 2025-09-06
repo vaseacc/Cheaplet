1cc2288a29a9c4d6c1ca1848f22cba5a7a287f8e
vaseacc <vase558558@gmail.com>
2025-09-06 12:34:50 -0400

    Create config.js

---
+++ netlify/functions/config.js
@@ -0,0 +1,21 @@
+// netlify/functions/config.js
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      firebaseConfig: {
+        apiKey: process.env.VITE_FIREBASE_API_KEY,
+        authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.VITE_FIREBASE_APP_ID,
+        measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID
+      },
+      cloudinary: {
+        cloudName: process.env.VITE_CLOUDINARY_CLOUD_NAME,
+        uploadPreset: process.env.VITE_CLOUDINARY_UPLOAD_PRESET
+      }
+    })
+  };
+};
