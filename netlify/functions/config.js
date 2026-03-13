fc821c36b0ba16bbbc984acf3ac98aeec48118bf
vaseacc <vase558558@gmail.com>
2026-03-12 20:39:26 -0400

    Update config.js

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -1,24 +1,17 @@
 exports.handler = async function(event, context) {
-  // We are removing the referer check temporarily to fix your site design
   return {
     statusCode: 200,
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*", // Allows your site to read it
-    },
     body: JSON.stringify({
-      firebaseConfig: {
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID,
-        measurementId: process.env.FIREBASE_MEASUREMENT_ID
+      // This will show us if Netlify even sees the keys at all
+      debug: {
+        hasApiKey: !!process.env.FIREBASE_API_KEY,
+        hasProjectId: !!process.env.FIREBASE_PROJECT_ID,
+        allKeys: Object.keys(process.env).filter(k => k.includes("FIREBASE"))
       },
-      cloudinary: {
-        cloudName: process.env.CLOUDINARY_CLOUD_NAME,
-        uploadPreset: process.env.CLOUDINARY_UPLOAD_PRESET
+      firebaseConfig: {
+        apiKey: process.env.FIREBASE_API_KEY || "NOT_FOUND",
+        projectId: process.env.FIREBASE_PROJECT_ID || "NOT_FOUND",
+        // ... add the rest if you want
       }
     })
   };
