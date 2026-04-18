d12774beb6452e48471ed581735b00031f746c67
vaseacc <vase558558@gmail.com>
2026-04-18 13:27:18 -0400

    Update sign-upload.js

--- netlify/functions/sign-upload.js
+++ netlify/functions/sign-upload.js
@@ -1,29 +1,37 @@
-const cloudinary = require('cloudinary').v2;
+// Cloudflare Pages Function: /functions/sign-upload.js
+export async function onRequest(context) {
+  const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "GET, OPTIONS",
+  };
 
-exports.handler = async (event, context) => {
-    const timestamp = Math.round((new Date()).getTime() / 1000);
-    
-    // Configure Cloudinary with your SECRET keys
-    cloudinary.config({
-        cloud_name: process.env.VITE_CLOUDINARY_CLOUD_NAME,
-        api_key: process.env.VITE_CLOUDINARY_API_KEY, 
-        api_secret: process.env.VITE_CLOUDINARY_API_SECRET 
-    });
+  if (context.request.method === "OPTIONS") {
+    return new Response(null, { headers: corsHeaders });
+  }
 
-    // Create the secure signature
-    const signature = cloudinary.utils.api_sign_request({
-        timestamp: timestamp,
-        upload_preset: process.env.VITE_CLOUDINARY_UPLOAD_PRESET,
-    }, process.env.VITE_CLOUDINARY_API_SECRET);
+  const env = context.env;
+  const cloudinary = require('cloudinary').v2;
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            signature: signature,
-            timestamp: timestamp,
-            apiKey: process.env.VITE_CLOUDINARY_API_KEY,
-            cloudName: process.env.VITE_CLOUDINARY_CLOUD_NAME,
-            uploadPreset: process.env.VITE_CLOUDINARY_UPLOAD_PRESET
-        })
-    };
-};
+  cloudinary.config({
+    cloud_name: env.VITE_CLOUDINARY_CLOUD_NAME,
+    api_key: env.VITE_CLOUDINARY_API_KEY,
+    api_secret: env.VITE_CLOUDINARY_API_SECRET
+  });
+
+  const timestamp = Math.round((new Date()).getTime() / 1000);
+  const signature = cloudinary.utils.api_sign_request({
+    timestamp: timestamp,
+    upload_preset: env.VITE_CLOUDINARY_UPLOAD_PRESET,
+  }, env.VITE_CLOUDINARY_API_SECRET);
+
+  return new Response(JSON.stringify({
+    signature,
+    timestamp,
+    apiKey: env.VITE_CLOUDINARY_API_KEY,
+    cloudName: env.VITE_CLOUDINARY_CLOUD_NAME,
+    uploadPreset: env.VITE_CLOUDINARY_UPLOAD_PRESET
+  }), {
+    headers: { "Content-Type": "application/json", ...corsHeaders }
+  });
+}
