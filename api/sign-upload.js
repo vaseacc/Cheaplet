405c687a280dfe8ae71ffcc3b4479e51217f9b9e
vaseacc <vase558558@gmail.com>
2026-04-22 16:56:54 -0400

    Change require to import for cloudinary module

--- api/sign-upload.js
+++ api/sign-upload.js
@@ -1,4 +1,4 @@
-const cloudinary = require('cloudinary').v2;
+import { v2 as cloudinary } from 'cloudinary';
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
