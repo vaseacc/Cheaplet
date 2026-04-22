544bea5dd2f8f5aeb4dc13d0ea701b5a40e79598
vaseacc <vase558558@gmail.com>
2026-04-21 20:54:08 -0400

    Refactor Cloudinary import and CORS handling

--- functions/api/sign-upload.js
+++ functions/api/sign-upload.js
@@ -1,26 +1,19 @@
 // functions/api/sign-upload.js
-import { v2 as cloudinary } from 'cloudinary';
+import cloudinary from 'cloudinary';
 
 export async function onRequest(context) {
   const { request, env } = context;
 
-  if (request.method === "OPTIONS") {
-    return new Response(null, {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type",
-      },
-    });
-  }
+  // ... CORS handling ...
 
-  cloudinary.config({
+  cloudinary.v2.config({
     cloud_name: env.VITE_CLOUDINARY_CLOUD_NAME,
     api_key: env.VITE_CLOUDINARY_API_KEY,
     api_secret: env.VITE_CLOUDINARY_API_SECRET
   });
 
   const timestamp = Math.round((new Date()).getTime() / 1000);
-  const signature = cloudinary.utils.api_sign_request({
+  const signature = cloudinary.v2.utils.api_sign_request({
     timestamp: timestamp,
     upload_preset: env.VITE_CLOUDINARY_UPLOAD_PRESET,
   }, env.VITE_CLOUDINARY_API_SECRET);
