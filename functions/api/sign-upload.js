83b01b52fdcc29e834f014c64a9fd118c68894e5
vaseacc <vase558558@gmail.com>
2026-04-21 20:51:12 -0400

    Refactor Cloudinary import and configuration

--- functions/api/sign-upload.js
+++ functions/api/sign-upload.js
@@ -1,4 +1,5 @@
-import cloudinary from 'cloudinary';
+// functions/api/sign-upload.js
+import { v2 as cloudinary } from 'cloudinary';
 
 export async function onRequest(context) {
   const { request, env } = context;
@@ -12,14 +13,14 @@ export async function onRequest(context) {
     });
   }
 
-  cloudinary.v2.config({
+  cloudinary.config({
     cloud_name: env.VITE_CLOUDINARY_CLOUD_NAME,
     api_key: env.VITE_CLOUDINARY_API_KEY,
     api_secret: env.VITE_CLOUDINARY_API_SECRET
   });
 
   const timestamp = Math.round((new Date()).getTime() / 1000);
-  const signature = cloudinary.v2.utils.api_sign_request({
+  const signature = cloudinary.utils.api_sign_request({
     timestamp: timestamp,
     upload_preset: env.VITE_CLOUDINARY_UPLOAD_PRESET,
   }, env.VITE_CLOUDINARY_API_SECRET);
